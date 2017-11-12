cdbbdba89838b6d240c348d45882a0cde56986a2
matt.anderson.inf <matt.anderson.inf@gmail.com>
2017-11-12 10:50:25 -0500

    information from the php script can be passed into the python script, need ot stylize email content

--- EmailBot.py
+++ EmailBot.py
@@ -1,25 +1,31 @@
 # email bot that will notify students that have a preference towards specific research opportunities
-import smtplib
-
+import smtplib, sys, json, pyodbc
 #class EmailBot(self):
 # this is the content of the email that you want to send
-email_content = 'this is an email.'
-# this connection line to the email server
-mail = smtplib.SMTP('smtp.gmail.com', 587)
-# this is how you identify yourself to the server
-mail.ehlo()
-# start tls(transport layer security) mode to encrypt email account
-mail.starttls()
 
-#mail.login('email user', 'password')
-mail.login('matt.anderson.inf@gmail.com', 'Hummingbird95.')
-
-# fromemail is the email that is going to be read that it was sent from
-# reciever : you're sending an email to said reciever
-# contents: email contains contents specified above
-mail.sendmail('matt.anderson.inf@gmail.com','mra97@scarletmail.rutgers.edu', email_content)
-
-print('An email was sent.')
+try:    # grab inputs from php script
+    inputs = list(sys.argv[1].split('')) 
+    
+    if inputs[0] == 'Faculty': email_content = 'insert path to faculty email content'
+    elif input[0] == 'Student': email_content = 'insert path to student email content'
+    else:
+    
+    # this connection line to the email server
+    mail = smtplib.SMTP('mail.njit.', 587)
+    # this is how you identify yourself to the server
+    mail.ehlo()
+    # start tls(transport layer security) mode to encrypt email account
+    mail.starttls()
+    
+    #mail.login('email user', 'password')
+    mail.login('cww5', 'cww5cww5')
+    
+    # fromemail is the email that is going to be read that it was sent from
+    # reciever : you're sending an email to said reciever
+    # contents: email contains contents specified above
+    mail.sendmail('matt.anderson.inf@gmail.com','mra97@scarletmail.rutgers.edu', email_content)
+    
+    print('An email was sent.')
     
     
     
